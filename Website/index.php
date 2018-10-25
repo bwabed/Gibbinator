9bcff03c8a389c2960c4250e894c535d617fa63b
Dimi <dimitri.waber@gmail.com>
2018-10-25 13:30:03 +0200

    Removed unused require once in index.php

--- Website/index.php
+++ Website/index.php
@@ -12,7 +12,6 @@
 require_once 'lib/Dispatcher.php';
 require_once 'lib/View.php';
 require_once 'lib/Model.php';
-require_once 'lib/formbuilder/FormBuilder.php';
 
 $dispatcher = new Dispatcher();
 $dispatcher->dispatch();
