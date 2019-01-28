a0586f4e79be6e3ddc3c0fd86d5321b078ad073a
Dimitri Waber <bwabed@users.noreply.github.com>
2019-01-28 17:44:12 +0100

    Visual bugfix in admin interface

--- view/admin_infra.php
+++ view/admin_infra.php
@@ -177,7 +177,7 @@
         </div>
     </div>
     <div class="mdl-card mdl-cell mdl-cell--7-col mdl-grid--no-spacing mdl-shadow--2dp">
-        <div class="mdl-cell mdl-cell--12-col mdl-card__title mdl-color--indigo-500">
+        <div class="mdl-cell--12-col mdl-card__title mdl-color--indigo-500">
             <h2 class="mdl-card__title-text mdl-color-text--white">Zimmer</h2>
         </div>
         <table class="customTable mdl-cell--12-col mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp"
